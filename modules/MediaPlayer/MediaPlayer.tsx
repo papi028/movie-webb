40bba280a78b429c550670420b105b5dd41792c9
lamhoang1256 <lamhoang1256@gmail.com>
2022-12-02 15:21:15 +0700

    Refactor: reuseable component WatchPage

--- modules/MediaPlayer/MediaPlayer.tsx
+++ modules/MediaPlayer/MediaPlayer.tsx
@@ -3,19 +3,28 @@ const ReactHlsPlayer = dynamic(() => import("react-hls-player"), {
 });
 import { IQuality, ISubtitle } from "@types";
 import dynamic from "next/dynamic";
+import { HlsPlayerProps } from "react-hls-player";
 import { Player } from "react-tuby";
 import "react-tuby/css/main.css";
 
-interface MediaPlayerProps {
+interface MediaPlayerProps extends Partial<HlsPlayerProps> {
   subtitles: ISubtitle[];
   qualities: IQuality[];
   poster: string;
 }
 
-const MediaPlayer = ({ subtitles, qualities, poster }: MediaPlayerProps) => {
+const MediaPlayer = ({
+  subtitles,
+  qualities,
+  poster,
+  playerRef,
+  ...HlsProps
+}: MediaPlayerProps) => {
   return (
-    <Player src={qualities} subtitles={subtitles}>
-      {(ref, props) => <ReactHlsPlayer playerRef={ref} {...props} autoPlay poster={poster} />}
+    <Player src={qualities} subtitles={subtitles} playerRef={playerRef}>
+      {(ref, props) => (
+        <ReactHlsPlayer {...props} {...HlsProps} playerRef={ref} autoPlay={false} poster={poster} />
+      )}
     </Player>
   );
 };
--- pages/watch/[category]/[id]/[episode].tsx
+++ pages/watch/[category]/[id]/[episode].tsx
@@ -1,99 +1,14 @@
 import { IEpisode } from "@types";
-import { Meta } from "components/Meta";
 import axiosClient from "configs/axiosClient";
-import { resizeImageLoklok } from "constants/global";
-import useSaveHistoryView from "hooks/useSaveHistoryView";
-import { LayoutPrimary } from "layouts/LayoutPrimary";
-import { MediaPlayer } from "modules/MediaPlayer";
-import { MovieCard } from "modules/MovieCard";
-import { MovieList } from "modules/MovieList";
-import { RelatedSeries } from "modules/RelatedSeries";
-import { WatchActions } from "modules/WatchActions";
-import { WatchAnthology } from "modules/WatchAnthology";
-import { WatchCategory } from "modules/WatchCategory";
-import { WatchMeta } from "modules/WatchMeta";
-import { WatchStar } from "modules/WatchStar";
-import { WatchSummary } from "modules/WatchSummary";
+import { WatchPage } from "modules/WatchPage";
 import { GetStaticPaths, GetStaticPropsContext } from "next";
-import { useEffect, useState } from "react";
-import styles from "styles/watch.module.scss";
-import classNames from "utils/classNames";
 
 interface WatchMoviePageProps {
   data: IEpisode;
 }
 
 const WatchMoviePage = ({ data }: WatchMoviePageProps) => {
-  useSaveHistoryView(data);
-  const [mounted, setMounted] = useState(false);
-  useEffect(() => {
-    setMounted(true);
-  }, []);
-  if (!mounted) {
-    return (
-      <Meta
-        title={`${data.name} - NetFilm`}
-        description={data.introduction}
-        image={resizeImageLoklok(data.coverHorizontalUrl, 800, 418)}
-      />
-    );
-  }
-  return (
-    <LayoutPrimary>
-      <Meta
-        title={`${data.name} - NetFilm`}
-        description={data.introduction}
-        image={resizeImageLoklok(data.coverHorizontalUrl, 800, 418)}
-      />
-      <div className="container">
-        <div className={styles.layout}>
-          <div className={styles.layoutMain}>
-            <MediaPlayer
-              qualities={data.qualities}
-              subtitles={data.subtitles}
-              poster={data.coverHorizontalUrl}
-            />
-            <h1 className={styles.heading}>
-              {data.name} {data.currentEpName && `- ${data.currentEpName}`}
-            </h1>
-            <div className={styles.meta}>
-              <WatchMeta
-                areaList={data.areaList}
-                currentEpisode={data.currentEpisode}
-                episodeCount={data.episodeCount}
-                year={data.year}
-                score={data.score}
-              />
-              <WatchActions
-                id={data.id}
-                title={data.name}
-                domainType={data.category}
-                poster={data.coverVerticalUrl}
-              />
-            </div>
-            <WatchCategory categories={data.tagList} />
-            <WatchSummary introduction={data.introduction} />
-            <WatchStar starList={data.starList} />
-          </div>
-          <div className={classNames(styles.layoutSidebar, "scrollbar")}>
-            <WatchAnthology detailMovie={data} />
-            <RelatedSeries refList={data.refList} />
-          </div>
-        </div>
-        <MovieList heading="You may like">
-          {data.likeList.map((movie: any) => (
-            <MovieCard
-              key={movie.id}
-              id={movie.id}
-              title={movie.name}
-              poster={movie.coverVerticalUrl}
-              domainType={movie.category}
-            />
-          ))}
-        </MovieList>
-      </div>
-    </LayoutPrimary>
-  );
+  return <WatchPage data={data} />;
 };
 
 export const getStaticPaths: GetStaticPaths = () => {
